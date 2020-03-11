ddb054550ff08bdd830a0c0ad8114d35254fb891
Eikyuu <v.duguet.dev@gmail.com>
2020-03-11 19:37:57 +0100

    ??

---
+++ src/app/authentication/signin/signin.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-signin',
+  templateUrl: './signin.component.html',
+  styleUrls: ['./signin.component.css']
+})
+export class SigninComponent implements OnInit {
+
+  signinForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.initSigninForm();
+  }
+
+  initSigninForm() {
+    this.signinForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmitSigninForm() {
+    console.log(this.signinForm.value);
+  }
+}
